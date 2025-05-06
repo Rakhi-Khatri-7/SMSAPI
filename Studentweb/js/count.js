b065a2b4a571201f590e2f957881cf98a981c8ee
Rakhi-Khatri-7 <bscs2312383@szabist.pk>
2025-05-06 12:56:14 +0000

    all

---
+++ Studentweb/js/count.js
@@ -0,0 +1,7 @@
+const count_countries = "https://miniature-rotary-phone-69wxj75w59p734469-5001.app.github.dev/count_countries";
+const count_dept = "https://miniature-rotary-phone-69wxj75w59p734469-5001.app.github.dev/count_dept";
+const count_emp = "https://miniature-rotary-phone-69wxj75w59p734469-5001.app.github.dev/count_emp";
+const count_jobhistory = "https://miniature-rotary-phone-69wxj75w59p734469-5001.app.github.dev/count_jobhistory";
+const count_jobs = "https://miniature-rotary-phone-69wxj75w59p734469-5001.app.github.dev/count_jobs";
+const count_locations = "https://miniature-rotary-phone-69wxj75w59p734469-5001.app.github.dev/count_locations";
+const count_reg = "https://miniature-rotary-phone-69wxj75w59p734469-5001.app.github.dev/count_reg";
